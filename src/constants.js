ce690361e7e5197ff927d026d018e3aaf1012cdc
adiXcodr <ichbinadittyadey@gmail.com>
2020-12-29 12:12:29 +0530

    ngrok

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
-const server_link = "http://localhost:9999/api";
-
+//const server_link = "http://localhost:9999/api";
+const server_link = "https://2f6432aec27a.ngrok.io/api";
+ 
 module.exports = server_link;
